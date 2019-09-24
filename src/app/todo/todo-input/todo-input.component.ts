280d41bb85275732763eb7d3d16c9852d98d60db
LendupLepcha <lenduplepcha62@gmail.com>
2019-09-24 07:33:03 +0530

    initial commit

--- src/app/todo/todo-input/todo-input.component.ts
+++ src/app/todo/todo-input/todo-input.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-todo-input',
   templateUrl: './todo-input.component.html',
   styleUrls: ['./todo-input.component.css']
 })
-export class TodoInputComponent implements OnInit {
+export class TodoInputComponent implements OnInit{
 
   constructor() { }
-  item=" ";
-  list=["one","two"];
+  list=[]
+  item=""
   show(){
     this.list.push(this.item);
   }
