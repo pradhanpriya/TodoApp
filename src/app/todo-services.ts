cf4e6cab9e702737336b1901b2b93ea1e0b53879
LendupLepcha <lenduplepcha62@gmail.com>
2019-09-30 22:27:08 +0530

    initial commit

--- src/app/todo-services.ts
+++ src/app/todo-services.ts
@@ -3,16 +3,12 @@ import { Injectable } from "@angular/core";
 
 export class todoService{
     list=[]
-    static  n=0;
     additem(item){
         this.list.push(item);
-        todoService.n=todoService.n+1;
     }
     
     getlist(){
         return this.list;
     }
-    getlen(){
-        return todoService.n;
-    }
+   
 }
